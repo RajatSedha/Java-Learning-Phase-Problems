e1c742995b48b39bad132ba1f0cf8f0728c3e52a
Rajat Sedha <80527626+RajatSedha@users.noreply.github.com>
2021-03-12 20:56:26 +0530

    Create EvenInterestDecrease.java
    using the for statement, call the calculateInterest method with the amount of 10000 with an interestRate of 8,7,6,5,4,3,2 and print the results to the console window.

---
+++ EvenInterestDecrease.java
@@ -0,0 +1,15 @@
+public class EvenInterest {
+    public static void main(String[] args) {
+        calculateInterest(10000);
+    }
+
+    public static void calculateInterest(int number) {
+        double interest = 0;
+        if (number >= 0) {
+            for (int i = 8; i > 0; i--) {
+                interest = (number * i) / 100.00;
+                System.out.println(interest);
+            }
+        }
+    }
+}
